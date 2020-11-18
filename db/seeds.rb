02f8e5ae6a12ca106295a192e1ae68ba57eab541
Ian Chin <ianchinbv@gmail.com>
2020-11-18 16:20:15 +1100

    all done no errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# if Rails.env.development?
+#     puts "Please don't seed data in production, you moron"
+#     return
+# eneeded
+require 'faker'
+
+puts 'Cleaning DB...'
+Restaurant.destroy_all
+
+puts "creating restaurants..."
+
+10.times do
+    Restaurant.create!(
+        name: Faker::Restaurant.name,
+        address: Faker::Address.full_address,
+        phone_number: Faker::PhoneNumber.phone_number,
+        category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+    )
+end
+puts "done!"
